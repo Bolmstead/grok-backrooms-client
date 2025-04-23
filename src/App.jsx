ca11c1e10e7e1b3191f1c5f9173c00e65693848f
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-04-22 21:06:17 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GrokConversation from "./components/GrokConversation";
 import ConversationText from "./components/ConversationText";
 import SearchPage from "./components/SearchPage";
 import NotFoundPage from "./components/NotFoundPage";
+import AsciiArtPage from "./components/AsciiArtPage";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<GrokConversation />} />
           <Route path="/archive" element={<SearchPage />} />
           <Route path="/conversation/:id" element={<ConversationText />} />
+          <Route path="/ascii" element={<AsciiArtPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
